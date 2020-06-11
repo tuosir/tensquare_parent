0de20db3a90b18c2a5752c3a7fff2476a2c76ff5
tuosir <tuozhiyong123@qq.com>
2020-06-11 20:36:13 +0800

    第一次提交

---
+++ tensquare_common/src/main/java/entity/PageResult.java
@@ -0,0 +1,16 @@
+package entity;
+
+import java.util.List;
+
+public class PageResult <T>{
+    private long total;
+    private List<T> rows;
+
+    public PageResult() {
+    }
+
+    public PageResult(long total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+}
